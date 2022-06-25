9b0b78c0441ce94f47d6145cef21a3ba3edff75b
Konstantin Mishur <kmishur@its.jnj.com>
2022-06-25 12:16:04 +0300

    Remove preval plugin, remove redundant files

--- packages/gatsby-theme-bodiless/gatsby-node.js
+++ packages/gatsby-theme-bodiless/gatsby-node.js
@@ -47,9 +47,6 @@ exports.onCreateBabelConfig = args => {
     name: '@babel/plugin-proposal-decorators',
     options: { legacy: true },
   });
-  setBabelPlugin({
-    name: 'babel-plugin-preval',
-  });
 };
 
 /**
